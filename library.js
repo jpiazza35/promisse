daf9252c812a03e883ed9e73a0b490afdca90fc9
jpiazza35 <36086658+jpiazza35@users.noreply.github.com>
2020-04-29 10:42:43 -0300

    Update library.js

--- library.js
+++ library.js
@@ -34,6 +34,37 @@ let cookTheBeans = (isSoftened) => {
     }, 1000)
   })
 }
+let cookBeans = () => {
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('beans')
+   }, 1000)
+ })
+}
+
+let steamBroccoli = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('broccoli')
+   }, 1000)
+ })
+}
+
+let cookRice = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('rice')
+   }, 1000)
+ })
+}
+
+let bakeChicken = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('chicken')
+   }, 1000)
+ })
+}
 
   
-module.exports = {shopForBeans, soakTheBeans, cookTheBeans} 
+module.exports = {cookBeans, steamBroccoli, cookRice, bakeChicken,shopForBeans, soakTheBeans, cookTheBeans} 
