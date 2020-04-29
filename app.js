1e1b78f3fc8b338c3de361bd396fa58fd0b3efa3
jpiazza35 <36086658+jpiazza35@users.noreply.github.com>
2020-04-29 10:44:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
-
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
 // Write your code below:
 
 
@@ -11,3 +11,18 @@ async function makeBeans (){
 }
 
 makeBeans()
+
+
+
+
+// Write your code below:
+
+async function serveDinner(){
+  const vegetablePromise =  steamBroccoli();
+  const starchPromise  =  cookRice();
+  const proteinPromise  =  bakeChicken();
+  const sidePromise  =  cookBeans();
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`)
+};
+
+serveDinner()
